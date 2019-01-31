d1219662b6bce84cb097239e1b19ffa32e4be65e
Bartosz <bartosz.kaszaa@gmail.com>
2019-01-31 18:58:34 +0100

    Project Object

---
+++ PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.springreactproject.ppmtool.repository;
+
+
+import com.springreactproject.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
