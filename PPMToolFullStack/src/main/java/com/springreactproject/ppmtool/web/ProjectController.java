e9fad6137fc12d9db022f63bda44aaaa244a27d5
Bartosz <bartosz.kaszaa@gmail.com>
2019-02-02 12:22:34 +0100

    Project Object

--- PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/web/ProjectController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,8 +22,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);//inaczej nie bedziemy wstanie wyswietlic w reaccie gdzie ejst blad
+        }
         Project project1=projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
