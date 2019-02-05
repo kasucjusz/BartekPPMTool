0eb8306e8c59edb8ea2a4ea3d4f245776ae5e24b
Bartosz <bartosz.kaszaa@gmail.com>
2019-02-06 00:39:45 +0100

    Find project by id

--- PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/services/ProjectService.java
@@ -30,4 +30,16 @@ public class ProjectService {
 
     }
 
+    public Project findProjectByIdentifier(String projectId){
+
+
+        Project project=projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null){
+            throw new ProjectIdException("Project ID "+ projectId+" doesn't exists");
+        }
+        else
+            return project;
+
+    }
+
 }
--- PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/springreactproject/ppmtool/web/ProjectController.java
@@ -10,10 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -40,6 +37,15 @@ public class ProjectController {
 
         Project project1=projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+        Project project=projectService.findProjectByIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
     }
 
 }
