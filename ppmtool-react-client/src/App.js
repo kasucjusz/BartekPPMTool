3fea0e7b37b2a55b0135167cbdf552a5545f9eb1
Bartosz <bartosz.kaszaa@gmail.com>
2019-02-08 12:32:54 +0100

    Added landing page to the dashboard

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Layout/Header';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
         <Dashboard />
 
       </div>
