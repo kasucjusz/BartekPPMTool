4cf7059e1cded1f0d2df535b2e9cf6a225144887
Bartosz <bartosz.kaszaa@gmail.com>
2019-02-24 22:43:00 +0100

    Finished working on all crud operations on react side of project

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem';
-import CreateProjectButton from './Project/CreateProjectButton';
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
+class Dashboard extends Component {
+  //another lifecycle hook
+  componentDidMount() {
+    this.props.getProjects();
+  }
 
-export default class Dashboard extends Component {
-    render() {
-        return (
-            <div className="projects">
+  render() {
+    const { projects } = this.props.project;
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h1 className="display-4 text-center">Projects</h1>
-                            <br />
-                            <CreateProjectButton />
-                            <br />
-                            <hr />
-
-                            <ProjectItem />
-
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
-
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import { getProject, createProject } from "../../actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { createProject } from "../../actions/projectActions";
 import classnames from "classnames";
 
-class AddProject extends Component {
+class UpdateProject extends Component {
   constructor() {
     super();
     this.state = {
+      id: "",
       projectName: "",
       projectIdentifier: "",
       description: "",
@@ -24,6 +25,29 @@ class AddProject extends Component {
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
     }
+    const {
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    } = nextProps.project;
+
+    this.setState({
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    });
+  }
+
+  //llifecycle hook
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
   }
 
   onChange(e) {
@@ -32,25 +56,26 @@ class AddProject extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    const newProject = {
+
+    const updateProject = {
+      id: this.state.id,
       projectName: this.state.projectName,
       projectIdentifier: this.state.projectIdentifier,
       description: this.state.description,
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
-    this.props.createProject(newProject, this.props.history);
-  }
 
+    this.props.createProject(updateProject, this.props.history);
+  }
   render() {
     const { errors } = this.state;
-
     return (
-      <div>
+      <div className="register">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <h5 className="display-4 text-center">Create Project form</h5>
+              <h5 className="display-4 text-center">Update Project Form</h5>
               <hr />
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
@@ -69,15 +94,13 @@ class AddProject extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className={classnames("form-control form-control-lg ", {
-                      "is-invalid": errors.projectIdentifier
-                    })}
+                    className="form-control form-control-lg"
                     placeholder="Unique Project ID"
                     name="projectIdentifier"
                     value={this.state.projectIdentifier}
                     onChange={this.onChange}
+                    disabled
                   />
-                  <p style={{ color: "red" }}>{errors.projectIdentifier}</p>
                 </div>
                 <div className="form-group">
                   <textarea
@@ -125,16 +148,19 @@ class AddProject extends Component {
   }
 }
 
-AddProject.propTypes = {
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
   createProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
+  project: state.project.project,
   errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
-  { createProject }
-)(AddProject);
+  { getProject, createProject }
+)(UpdateProject);
